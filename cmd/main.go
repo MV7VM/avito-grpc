487fba3758df91c9c4f663c8d5a87df868532b4c
max <kekovmaksim28@gmail.com>
2023-11-11 17:12:57 +0300

    grps final edition

--- cmd/main.go
+++ cmd/main.go
@@ -5,35 +5,92 @@ import (
 	"avito/internal/handlers"
 	"avito/internal/repository"
 	"avito/internal/service"
+	"avito/proto/proto/pb"
+	"context"
 	"fmt"
-	"github.com/gofiber/fiber/v2"
+	//"github.com/gofiber/fiber/v2/log"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log/slog"
+	"net"
+	"net/http"
+	"os"
 )
 
 type App struct {
-	routers    *fiber.App
 	repository *repository.Repository
 	service    *service.Service
 	handlers   *handlers.Handlers
 }
 
+const (
+	grpcPort = ":8090"
+	httpPort = ":8080"
+)
+
 func main() {
 	fmt.Println("Start")
-	//logger := slog.Logger{} //засунуть в контекст
+	//ctx := context.Background()
+	lis, err := net.Listen("tcp", grpcPort)
+	log := slog.New(slog.NewJSONHandler(os.Stdout, nil)) //засунуть в контекст
 	app := &App{}
-	app.routers = fiber.New()
 	cfg := config.Config_load()
 	app.repository = repository.New(cfg)
 	go func() {
 		err := app.repository.CacheRecovery()
 		if err != nil {
-			fmt.Println("Fail in cache recovery", err)
+			log.Error("Fail in cache recovery: " + err.Error())
 		}
 	}()
 	app.service = service.New(app.repository)
 	app.handlers = handlers.New(app.service)
-	app.routers.Post("/", app.handlers.Post)
-	app.routers.Get("/:Link", app.handlers.Get)
-	err := app.routers.Listen(":3000")
+	s := grpc.NewServer()
+	pb.RegisterYourServiceServer(s, app.handlers)
+	go func() {
+		if err = s.Serve(lis); err != nil {
+			log.Error("failed to serve: " + err.Error())
+			//log.Error("failed to serve: " + err.Error())
+		}
+	}()
+
+	conn, err := grpc.DialContext(
+		context.Background(),
+		grpcPort,
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Error("failed to dial serve: " + err.Error())
+		//log.Error("Failed to dial server: " + err.Error())
+	}
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			log.Error("failed to close connection " + err.Error())
+		}
+	}(conn)
+
+	gwmux := runtime.NewServeMux()
+	err = pb.RegisterYourServiceHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Error("failed : " + err.Error())
+		log.Error("Failed to register gateway:" + err.Error())
+	}
+
+	gwServer := &http.Server{
+		Addr:    httpPort,
+		Handler: gwmux,
+	}
+
+	log.Info("Serving gRPC-Gateway on port " + httpPort)
+	if err = gwServer.ListenAndServe(); err != nil {
+		if err == http.ErrServerClosed {
+			log.Warn("Server closed: " + err.Error())
+			os.Exit(0)
+		}
+		log.Error("Failed to listen and serve: " + err.Error())
+	}
 	if err != nil {
 		return
 	}
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"avito/proto/proto/pb"
+	"context"
 	"fmt"
-	"github.com/gofiber/fiber/v2"
-	"net/http"
+	"log/slog"
+	"os"
 )
 
 type service interface {
@@ -13,40 +15,49 @@ type service interface {
 
 type Handlers struct {
 	service
+	pb.UnimplementedYourServiceServer
 }
 
 func New(service service) *Handlers {
-	return &Handlers{service}
+	return &Handlers{service, pb.UnimplementedYourServiceServer{}}
 }
-func (h *Handlers) Post(c *fiber.Ctx) error {
+
+//func (h *Handlers) Create(ctx context.Context, req *pb.CreateUrlRequest) (*pb.CreateUrlResponse, error) {
+//	log := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+//	originalUrl := req.Url
+//	fmt.Println("Post - ", originalUrl)
+//	shortUrl, err := h.service.Create(originalUrl)
+//	if err != nil {
+//		return nil, status.Error(http.StatusBadRequest, "Bad request")
+//		log.Error("Inserting problems" + err.Error())
+//		//return nil, err
+//	}
+//	return &pb.CreateUrlResponse{ShortUrl: shortUrl}, nil
+//}
+
+func (h *Handlers) Create(ctx context.Context, request *pb.CreateUrlRequest) (*pb.CreateUrlResponse, error) {
 	// Read the param noteId
 	//link := c.Params("Link")
 	//var shortUrl model.Links
-	type shortLink struct {
-		ShortUrl string `json:"shortUrl"`
-	}
-	var shortUrl shortLink
-	err := c.BodyParser(&shortUrl)
+	log := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	longUrl := request.Url
+	fmt.Println("Post - ", longUrl)
+	msg, err := h.service.Create(longUrl)
 	if err != nil {
-		//log.Fatal(err)
-		return err
+		return nil, err
+		log.Error("error in servise: " + err.Error())
 	}
-	fmt.Println("Post - ", shortUrl.ShortUrl)
-	msg, err := h.service.Create(shortUrl.ShortUrl)
-	if err != nil {
-		return c.SendStatus(http.StatusBadRequest)
-	}
-	return c.SendString(msg)
+	return &pb.CreateUrlResponse{ShortUrl: msg}, nil
 	// Find the note with the given id
 	// Return the note with the id
 }
 
-func (h *Handlers) Get(c *fiber.Ctx) error {
-	link := c.Params("Link")
+func (h *Handlers) Get(ctx context.Context, request *pb.GetUrlRequest) (*pb.GetUrlResponse, error) {
+	link := request.Url
 	fmt.Println("Get - ", link)
 	msg, err := h.service.Get(link)
 	if err != nil {
-		return c.SendStatus(http.StatusNotFound)
+		return nil, err
 	}
-	return c.SendString(msg)
+	return &pb.GetUrlResponse{OriginalUrl: msg}, nil
 }
